5c245c389178108f6b1358b4e614dd4e6c7b81cf
drgnlrd <burhanhp5158@gmail.com>
2022-02-27 10:59:11 +0530

    complete

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -5,28 +5,21 @@ $(function () {
    */
   'use strict';
   var data = {
-    labels: ["2013", "2014", "2014", "2015", "2016", "2017"],
+    labels: ["Achieved", "Breached"],
+    indexAxis: "y",
     datasets: [{
       label: '# of Votes',
-      data: [10, 19, 3, 5, 2, 3],
+      data: [10, 19,],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
       ],
       borderColor: [
         'rgba(255,99,132,1)',
         'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
       ],
-      borderWidth: 1,
-      fill: false
+      borderWidth: 2,
+      fill: true
     }]
   };
   var dataDark = {
@@ -166,7 +159,7 @@ $(function () {
     animation: {
       animateScale: true,
       animateRotate: true
-    }
+    },
   };
   var areaData = {
     labels: ["2013", "2014", "2015", "2016", "2017"],
@@ -453,8 +446,17 @@ $(function () {
     }
   }
   // Get context with jQuery - using jQuery's .get() method.
-  if ($("#barChart").length) {
-    var barChartCanvas = $("#barChart").get(0).getContext("2d");
+  if ($("#barChart1").length) {
+    var barChartCanvas = $("#barChart1").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+    var barChart = new Chart(barChartCanvas, {
+      type: 'bar',
+      data: data,
+      options: options
+    });
+  }
+  if ($("#barChart2").length) {
+    var barChartCanvas = $("#barChart2").get(0).getContext("2d");
     // This will get the first returned node in the jQuery collection.
     var barChart = new Chart(barChartCanvas, {
       type: 'bar',
@@ -477,7 +479,7 @@ $(function () {
     var lineChartCanvas = $("#lineChart").get(0).getContext("2d");
     var lineChart = new Chart(lineChartCanvas, {
       type: 'line',
-      data: data,
+      data: dataDark,
       options: options
     });
   }
@@ -513,6 +515,14 @@ $(function () {
     var doughnutChartCanvas = $("#doughnutChart").get(0).getContext("2d");
     var doughnutChart = new Chart(doughnutChartCanvas, {
       type: 'doughnut',
+      data: doughnutPieData,
+      options: doughnutPieOptions
+    });
+  }
+  if ($("#doughnutChart2").length) {
+    var doughnutChartCanvas = $("#doughnutChart2").get(0).getContext("2d");
+    var doughnutChart = new Chart(doughnutChartCanvas, {
+      type: 'pie',
       data: doughnutPieData,
       options: doughnutPieOptions
     });
